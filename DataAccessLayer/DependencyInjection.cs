d761a075a706ed9b8cbc739fc1944cf171c80d2c
Pradeep Verma <pradeepnetcore@outlook.com>
2025-11-24 21:15:09 +0530

    Added boilerplate code in this project

---
+++ DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public static class DependencyInjection
+    {
+        // Extension methods for setting up data access layer dependencies would go here
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+        {
+            // Example: services.AddDbContext<MyDbContext>(options => ...);
+            // Add Data Access Layer services here to IOC Container
+
+            return services;
+        }
+    }
+}
