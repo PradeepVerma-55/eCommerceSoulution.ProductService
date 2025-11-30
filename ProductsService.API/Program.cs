61f2cedcaf747bacd720b099764e4ae511482793
Pradeep Verma <pradeepnetcore@outlook.com>
2025-11-30 10:53:29 +0530

    Updated - Swagger configuration

--- ProductsService.API/Program.cs
+++ ProductsService.API/Program.cs
@@ -18,7 +18,15 @@ builder.Services.AddFluentValidationAutoValidation();
 
 //Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "eCommerce Products API",
+        Version = "v1",
+        Description = "API for managing Products"
+    });
+});
 
 
 //Cors
@@ -34,19 +42,27 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
+
+
+
+
 app.UseRouting();
 
 //Cors
 app.UseCors();
 
-//Swagger
+
+//Swagger - should be early in the pipeline
+// Swagger - always enabled
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "eCommerce Product API v1");
-    c.RoutePrefix = string.Empty; // serve Swagger UI at app root
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "eCommerce Products API v1");
+    c.RoutePrefix = string.Empty;  // Serve at root
 });
 
+// Redirect root to Swagger if needed
+app.MapGet("/", () => Results.Redirect("/index.html")).ExcludeFromDescription();
 
 //Auth
 app.UseAuthentication();
